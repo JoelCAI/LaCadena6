765f0d786c5827d7471d236bdc5d0e76de9e2cba
JoelCAI <joelargentina12345@gmail.com>
2022-04-04 11:38:14 -0300

    Agregar archivos de proyecto.

---
+++ LaCadena6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LaCadena6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Sistema s = new Sistema();
+			s.Iniciar();
+		}
+
+	}
+}
